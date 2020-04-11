54bd74e3963c170c121fd888b1158e814b171815
Michael West <mpwest929+github@gmail.com>
2020-04-11 12:16:26 -0400

    Implement Debugger Console and CLI

---
+++ src/app/debugger_console.ts
@@ -0,0 +1,71 @@
+import { Gameboy, ZERO_FLAG, SUBTRACTION_FLAG, HALF_CARRY_FLAG, CARRY_FLAG } from "./emulator";
+import * as readlineSync from "readline-sync";
+
+export class DebugConsole {
+    private gameboy: Gameboy;
+    private breakpoints: number[];
+
+    constructor(gb: Gameboy) {
+        this.gameboy = gb;
+        this.breakpoints = [];
+    }
+
+    public addBreakpoint(addr: number) {
+        this.breakpoints.push(addr);
+    }
+
+    public breakpointTriggered(currAddr: number) {
+        return currAddr in this.breakpoints;
+    }
+
+    //private executeCommand(command: string) {
+    //    if (command === "p cpu") {
+    public displayCPUData() {
+        console.log(`
+        ---------------------- CPU ---------------------
+        PC = ${this.gameboy.cpu.PC}
+        A = ${this.gameboy.cpu.A}
+        B = ${this.gameboy.cpu.B}
+        C = ${this.gameboy.cpu.C}
+        D = ${this.gameboy.cpu.D}
+        E = ${this.gameboy.cpu.E}
+        SP = ${this.gameboy.cpu.SP}
+        ZeroFlag = ${this.gameboy.cpu.getFlag(ZERO_FLAG)}
+        SubtractionFlag = ${this.gameboy.cpu.getFlag(SUBTRACTION_FLAG)}
+        HalfCarry = ${this.gameboy.cpu.getFlag(HALF_CARRY_FLAG)}
+        CarryFlag = ${this.gameboy.cpu.getFlag(CARRY_FLAG)}
+        ---------------------- CPU ---------------------
+        `);
+    }
+    //}
+
+    public showConsole() {
+        //process.stdin.setRawMode(true);
+        /*readlineSync.promptCLLoop({
+            "p cpu": () => {
+                this.displayCPUData();
+            },
+            "c": () => {
+                return true;
+            },
+            "continue": () => {
+                return true;
+            }
+        });*/
+        /*
+          NOTE: By default readlineSync (or readline) will not process enter keys properly. Pressing enter displays the meta character.
+        */
+        readlineSync.emitKeypressEvents(process.stdin);
+        let showDebugger = true;
+        while (showDebugger) {
+            const command = readlineSync.question(`(h for help, c for continue) [${this.gameboy.cpu.PC}]> `, {hideEchoBack: false});
+            // this.executeCommand(command);
+
+            if (command === "p cpu") {
+                this.displayCPUData();
+            } else if (command === "c" || command === "continue") {
+                showDebugger = false;
+            }
+        }
+    } 
+}
